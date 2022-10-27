6844bd64617da60fff56dbe51825ff07fd56041c
Tamara Lawlor <lawlortamara@gmail.com>
2022-10-27 12:19:45 +0100

    fix: :fire: Removed redundant code causing build failure
    removed app config file and removed unnecessary code in HelloController.java and HelloControllerTest.java. Also added HelloControllerIT.java file

--- src/test/java/com/group_22235/HelloControllerTest.java
+++ src/test/java/com/group_22235/HelloControllerTest.java
@@ -1,48 +1,29 @@
 package com.group_22235;
 
-import com.jayway.restassured.RestAssured;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.RestAssured.when;
-import static org.hamcrest.CoreMatchers.is;
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = CS4125ProjectApplication.class)
-@WebAppConfiguration
-@SpringBootTest({"server.port:0",
-        "spring.datasource.url:jdbc:h2:mem:cs4125_project;DB_CLOSE_ON_EXIT=FALSE"})
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
-    @Value("${local.server.port}")
-    int port;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        RestAssured.port = port;
-    }
+	@Autowired
+	private MockMvc mvc;
 
-    @Test
-    public void testHello() throws Exception {
-        when().get("/").then()
-                .body(is("Hello World!"));
-    }
-
-    @Test
-    public void testCalc() throws Exception {
-        given().param("left", 100)
-                .param("right", 200)
-                .get("/calc")
-                .then()
-                .body("left", is(100))
-                .body("right", is(200))
-                .body("answer", is(300));
-    }
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+	}
 }
