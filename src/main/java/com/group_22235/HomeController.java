823c1c0055af90bb50c09641a15b09d1d914422c
Rachel <rachel.w.odon@gmail.com>
2022-11-11 21:42:18 -0800

    feat: :sparkles: HTML/CSS files base code
    Setting up the base of the HTML files along with the CSS files.

--- src/main/java/com/group_22235/HomeController.java
+++ src/main/java/com/group_22235/HomeController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HomeController {
 
-    @RequestMapping("/")
-    String hello() {
-        return "Hello World!";
+    @RequestMapping(value = "/index")
+    public String index() {
+       return "index";
     }
 }
