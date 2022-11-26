c33659c8ddf953d308ab3464da4b0e630b57733a
Tamara Lawlor <lawlortamara@gmail.com>
2022-11-26 15:52:11 +0000

    feat: :sparkles: Added Payment package classes which implement a strategy pattern
    added paymet strategy interface, an abstract payment class and payment strategy types creditcard and paypal

---
+++ src/main/java/com/group_22235/payment/CreditCardStrategy.java
@@ -0,0 +1,36 @@
+package com.group_22235.payment;
+
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "CREDIT_CARD")
+public class CreditCardStrategy extends APayment{
+
+    @Column
+    private String cardType;
+    @Column
+    private String cardName;
+    @Column
+    private int cardNumber;
+    @Column
+    private String expiryDate;
+    @Column
+    private int cvv;
+
+    public CreditCardStrategy(String cardType, String cardName, int cardNumber, String expiryDate, int cvv) {
+        this.cardType = cardType;
+        this.cardName = cardName;
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println(amount +" paid with " + cardType + " card");
+    }
+    
+}
--- src/main/java/com/group_22235/user/Member.java
+++ src/main/java/com/group_22235/user/Member.java
@@ -1,5 +1,8 @@
 package com.group_22235.user;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -7,6 +10,9 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.group_22235.payment.IPaymentStrategy;
+import com.group_22235.payment.Subscription;
 
 @Entity
 @Table(name = "MEMBER")
@@ -19,13 +25,29 @@ public class Member extends User {
     @Column(name = "travel_points")
     int travelPoints;
 
-    public Member(String name, String email, String password) {
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "subscription_id", nullable = false)
+    Subscription subscription;
+
+    public Member(String name, String email, String password, int paymentPlan) {
         super(name, email);
         acc = new Account(password, false);
+        subscription = new Subscription(paymentPlan);
     }
 
     public void calcDiscount(){
 
     }
     
+    public void paySubscription(IPaymentStrategy paymentMethod){
+		double amount = subscription.getCost();
+		paymentMethod.pay(amount);
+
+        Date oldRenewal = subscription.getRenewalDate();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(oldRenewal);
+        cal.add(Calendar.MONTH, subscription.getPaymentPlan());
+        subscription.setRenewalDate(cal.getTime());
+	}
+
 }
