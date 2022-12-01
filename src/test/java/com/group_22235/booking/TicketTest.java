6c97ff435c03568f32eb45d272f1a5138024cc08
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2022-12-01 01:26:01 +0000

    feat: :sparkles: Added more coverage tests
    Added more tests to cover all aspects of the code

--- src/test/java/com/group_22235/booking/TicketTest.java
+++ src/test/java/com/group_22235/booking/TicketTest.java
@@ -14,21 +14,22 @@ class TicketTest {
 
 	// @Autowired
     // private TicketService tService;
+
+	LocalTime l = LocalTime.now();
+	LocalDate d = LocalDate.now();
+
+	Station s = new Station("Town", "Letterkenny");
+	Station s2 = new Station("Connolly", "Cork");
+
 	@Test
-	void should_test_something(){
-
-		LocalTime l = LocalTime.now();
-		LocalDate d = LocalDate.now();
-
-	    Station s = new Station("Town", "Letterkenny");
-		Station s2 = new Station("Connolly", "Cork");
+	void should_generate_young_adult_first_class(){
 
 			TicketFacade a = new TicketFacade(s, s2, l, d, "FIRST");
 		
 			System.out.println(s.getLocation() + "-> " + s2.getLocation());
 			System.out.println(a.getTicketPrice());
 			a.setYAPrice();
-			System.out.println("Adult price + price: " + a.getYAPrice());
+			System.out.println("Young Adult price: " + a.getYAPrice());
 			System.out.println(a.getTimeLimit());
 			System.out.println(a.getFirstClass());
 			System.out.println(a.getDate());
@@ -38,4 +39,34 @@ class TicketTest {
 			
 	}
 
+	@Test
+	void should_generate_child_flexible(){
+
+		TicketFacade b = new TicketFacade(s, s2, l, d, "FLEX");
+
+		b.getDepStation();
+		b.getArrStation();
+		b.setChildPrice();
+		b.getChildPrice();
+	}
+
+	@Test
+	void should_generate_adult_semi(){
+
+		TicketFacade c = new TicketFacade(s, s2, l, d, "SEMI");
+
+		c.setAdultPrice();
+		c.getAdultPrice();
+		c.getTicketType();
+	}
+
+	@Test
+	void should_generate_oap_first_class(){
+
+		TicketFacade dd = new TicketFacade(s, s2, l, d, "FIRST");
+
+		dd.setOAPPrice();
+		dd.getOAPPrice();
+
+	}
 }
